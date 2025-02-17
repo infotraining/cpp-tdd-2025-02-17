72c57b6263c234bb1977df59a6739957a6d1b2d3
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-02-17 10:27:01 +0100

    BowlingGame - step 2

--- tdd-katas/gtest/bowling/tests/bowling_tests.cpp
+++ tdd-katas/gtest/bowling/tests/bowling_tests.cpp
@@ -17,11 +17,26 @@ public:
     {
         return 0;
     }
+
+    void roll(size_t pins) 
+    {
+
+    }
 };
 
 TEST(BowlingGameTests, When_GameStarts_ScoreIsZero)
 {
     BowlingGame game;
+
+    ASSERT_EQ(game.score(), 0);
+}
+
+TEST(BowlingGameTests, When_AllRollsInGutter_ScoreIsZero)
+{
+    BowlingGame game;
+
+    for(int i = 0; i < 20; ++i)
+        game.roll(0);
 
     ASSERT_EQ(game.score(), 0);
 }
