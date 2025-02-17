b4593ed012b7a6fea23e501332f29eb875f545f7
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-02-17 14:31:48 +0100

    BowlingGame - finished

--- tdd-katas/gtest/bowling/src/bowling.hpp
+++ tdd-katas/gtest/bowling/src/bowling.hpp
@@ -1,14 +1,86 @@
 #ifndef BOWLING_HPP
 #define BOWLING_HPP
 
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <tuple>
+#include <stdexcept>
+#include <cassert>
 
 namespace TDD
 {
-    class BowlingGame   
-    {};
+    class BowlingGame
+    {
+    public:
+        size_t score() const
+        {
+            size_t sum{};
+
+            for (int frame_index = 0, i = 0; frame_index < 10; ++frame_index)
+            {
+                auto [current_frame_score, frame_size] = frame_score(i);
+                sum += current_frame_score;
+
+                if (is_strike(i))
+                    sum += strike_bonus(i);
+                else if (is_spare(i))
+                    sum += spare_bonus(i);
+
+                i += frame_size;
+            }
+
+            return sum;
+        }
+
+        void roll(size_t pins)
+        {
+            if (pins > number_of_pins_in_frame)
+                throw std::invalid_argument("Cannot knock down more than 10 pins");
+
+            assert(m_roll_index < m_rolls.size());
+
+            m_rolls[m_roll_index] = pins;
+            ++m_roll_index;
+        }
+
+        constexpr static size_t number_of_pins_in_frame = 10;
+
+    private:
+        size_t m_score{0};
+        size_t m_roll_index{};
+        std::array<size_t, 22> m_rolls{};
+
+        std::tuple<size_t, size_t> frame_score(size_t roll_index) const
+        {
+            if (is_strike(roll_index))
+                return {number_of_pins_in_frame, 1};
+
+            return {m_rolls[roll_index] + m_rolls[roll_index + 1], 2};
+        }
+
+        bool is_spare(size_t roll_index) const
+        {
+            return m_rolls[roll_index] + m_rolls[roll_index + 1] == number_of_pins_in_frame;
+        }
+
+        size_t spare_bonus(size_t roll_index) const
+        {
+            return m_rolls[roll_index + 2];
+        }
+
+        bool is_strike(size_t roll_index) const
+        {
+            return m_rolls[roll_index] == number_of_pins_in_frame;
+        }
+
+        size_t strike_bonus(size_t roll_index) const
+        {
+            return m_rolls[roll_index + 1] + m_rolls[roll_index + 2];
+        }
+    };
 }
 
 #endif
--- tdd-katas/gtest/bowling/tests/bowling_tests.cpp
+++ tdd-katas/gtest/bowling/tests/bowling_tests.cpp
@@ -12,105 +12,7 @@
 
 using namespace std;
 
-class BowlingGame
-{
-public:
-    size_t score() const
-    {
-        size_t sum{};
-
-        for(int frame_index = 0, i = 0; frame_index < 10; ++frame_index)
-        {
-            auto [current_frame_score, frame_size] = frame_score(i);
-            sum += current_frame_score;
-
-            if (is_strike(i) )
-                sum += strike_bonus(i);
-            else if (is_spare(i))
-                sum += spare_bonus(i);
-                
-            i += frame_size;
-        }
-
-        return sum;
-    }
-
-    void roll(size_t pins) 
-    {
-        if (pins > number_of_pins_in_frame)
-            throw std::invalid_argument("Cannot knock down more than 10 pins");
-            
-        assert(m_roll_index < m_rolls.size());
-
-        m_rolls[m_roll_index] = pins;
-        ++m_roll_index;
-    }
-
-    constexpr static size_t number_of_pins_in_frame = 10;
-private:
-    size_t m_score {0};
-    size_t m_roll_index{};
-    std::array<size_t, 22> m_rolls{};
-
-    std::tuple<size_t, size_t> frame_score(size_t roll_index) const
-    {
-        if (is_strike(roll_index))
-            return {number_of_pins_in_frame, 1};
-        
-        return {m_rolls[roll_index] + m_rolls[roll_index + 1], 2};
-    }
-
-    bool is_spare(size_t roll_index) const
-    {
-        return m_rolls[roll_index] + m_rolls[roll_index + 1] == number_of_pins_in_frame;
-    }
-
-    size_t spare_bonus(size_t roll_index) const
-    {
-        return m_rolls[roll_index + 2];
-    }
-
-    bool is_strike(size_t roll_index) const
-    {
-        return m_rolls[roll_index] == number_of_pins_in_frame;
-    }
-
-    size_t strike_bonus(size_t roll_index) const
-    {
-        return m_rolls[roll_index + 1] + m_rolls[roll_index + 2];
-    }
-};
-
-namespace Alt1
-{
-    class BowlingGame
-    {
-    public:
-        size_t score() const
-        {
-            return std::accumulate(std::begin(m_rolls), std::end(m_rolls), 0ULL);
-        }
-   
-        void roll(size_t pins)
-        {
-            if (m_rolls.size() > 2)
-            {
-                auto index = m_rolls.size() - 1;
-                if (m_rolls[index] + m_rolls[index - 1] == 10) //spare
-                {
-                    pins *= 2;
-                }
-            }
-   
-            m_rolls.emplace_back(pins);
-        }
-   
-    private:
-        size_t m_score{};
-   
-        vector<size_t> m_rolls{};
-    };
-}
+using namespace TDD;
 
 class BowlingGameTests : public ::testing::Test
 {
@@ -227,3 +129,40 @@ TEST_F(BowlingGameTests, PerfectGame_ScoreIs300)
     
     ASSERT_EQ(game.score(), 300);
 }
+
+struct BowlingGameParams
+{
+    const char* test_description;
+    std::vector<size_t> rolls;
+    size_t expected_score;
+};
+
+std::ostream& operator<<(std::ostream& out, const BowlingGameParams& p)
+{
+    out << p.test_description;
+    return out;
+}  
+
+struct BowlingGameParamTests : ::testing::TestWithParam<BowlingGameParams>
+{
+    BowlingGame game; // SUT
+};
+
+TEST_P(BowlingGameParamTests, RealGameExamplesWithScore)
+{
+    const BowlingGameParams param = GetParam();    
+
+    for(size_t pins : param.rolls)
+        game.roll(pins);
+
+    ASSERT_EQ(game.score(), param.expected_score) << param.test_description;
+}
+
+BowlingGameParams params[] = {
+    { "simple game - all rolls one pin", { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 20 },
+    { "simple game - different rolls", {0, 8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 27},
+    { "strike & spare", {10, 4, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 47},
+    { "all spares & strike", {1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 10}, 119}
+};
+
+INSTANTIATE_TEST_SUITE_P(PackOfBowlingTests, BowlingGameParamTests, ::testing::ValuesIn(params));
