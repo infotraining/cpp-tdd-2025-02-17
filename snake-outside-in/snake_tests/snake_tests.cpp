af065310b4f324d7990a0d7a9b8d91f83b754e77
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-02-24 15:02:47 +0100

    Snake - test 3 - game over

--- snake-outside-in/snake_tests/snake_tests.cpp
+++ snake-outside-in/snake_tests/snake_tests.cpp
@@ -23,17 +23,19 @@ struct Board
 
 
 class TerminalParam;
+class SnakeParam;
 
-template <typename TTerminal = class TerminalParam>
+template <typename TTerminal = class TerminalParam, typename TSnake = SnakeParam>
 class SnakeGame
 {
-    enum class GameState { Menu, Playing };
+    enum class GameState { Menu, Playing, GameOver };
 
     TTerminal& terminal_;
+    TSnake& snake_;
     GameState game_state_{GameState::Menu};
 
 public:
-    explicit SnakeGame(TTerminal& terminal) : terminal_{terminal}
+    explicit SnakeGame(TTerminal& terminal, TSnake& snake) : terminal_{terminal}, snake_{snake}
     {}
     
     void run()
@@ -55,7 +57,15 @@ public:
     }
 private:
     void update()
-    {}
+    {
+        if (game_state_ == GameState::Playing)
+        {
+            if (!snake_.is_alive())
+            {
+                game_state_ = GameState::GameOver;            
+            }
+        }
+    }
 
     void render()
     {
@@ -66,6 +76,9 @@ private:
                 break;
             case GameState::Playing:
                 render_game();
+                break;
+            case GameState::GameOver:
+                render_game_over();
                 break;
         }
     }
@@ -78,9 +91,14 @@ private:
     void render_game()
     {
         terminal_.clear();
-        terminal_.render_snake(Snake{});
+        terminal_.render_snake(snake_);
         terminal_.render_board(Board{});
         terminal_.flush();
+    }
+
+    void render_game_over()
+    {
+        terminal_.render_text({"Game Over!!!"});
     }
 };
 
@@ -88,15 +106,42 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+template <typename TSnake = class SnakeParam>
 struct MockTerminal
 {
     MAKE_MOCK1(render_text,  auto (const std::vector<std::string>&) -> void);
     MAKE_MOCK0(read_key,     auto () -> std::optional<Key>);
     MAKE_MOCK0(clear,        auto () -> void);
-    MAKE_MOCK1(render_snake, auto (const Snake&) -> void);
+    MAKE_MOCK1(render_snake, auto (const TSnake&) -> void);
     MAKE_MOCK1(render_board, auto (const Board&) -> void);
     MAKE_MOCK0(flush,        auto () -> void);
 };
+
+
+template <typename TSnake>
+[[no_discard]] auto set_default_expectations(MockTerminal<TSnake>& terminal)
+{
+    return std::tuple{
+        NAMED_ALLOW_CALL(terminal, read_key()).RETURN(std::nullopt),
+        NAMED_ALLOW_CALL(terminal, clear()),
+        NAMED_ALLOW_CALL(terminal, render_text(ANY(std::vector<std::string>))),
+        NAMED_ALLOW_CALL(terminal, render_board(ANY(Board))),
+        NAMED_ALLOW_CALL(terminal, render_snake(ANY(TSnake))),
+        NAMED_ALLOW_CALL(terminal, flush())
+    };
+}
+
+struct MockSnake
+{
+    MAKE_MOCK0(is_alive, auto () -> bool);
+};
+
+[[no_discard]] auto set_default_expectations(MockSnake& snake)
+{
+    return std::tuple{
+        NAMED_ALLOW_CALL(snake, is_alive()).RETURN(true),
+    };
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -104,22 +149,34 @@ CATCH_REGISTER_ENUM(Key, Key::P, Key::Q, Key::ArrowUp, Key::ArrowDown, Key::Arro
 
 CATCH_REGISTER_ENUM(Direction, Direction::Up, Direction::Down, Direction::Left, Direction::Right)
 
+namespace di = boost::di;
+
+auto default_injector = [] {
+    return di::make_injector(
+        di::bind<class TerminalParam>.to<MockTerminal>(),
+        di::bind<class SnakeParam>.to<MockSnake>()
+    );
+};
+
 TEST_CASE("SnakeGame - rendering menu") 
 {
-    MockTerminal mock_terminal;
+    auto injector = default_injector();
+
+    using MockTerminalType = decltype(injector.create<MockTerminal>());
+    auto mock_terminal = injector.create<std::shared_ptr<MockTerminalType>>();
 
     std::vector<std::string> menu_items = {"~~Snake~~", "Play game [P]", "Quit [Q]"};
 
-    REQUIRE_CALL(mock_terminal, read_key()).RETURN(Key::Q);
-    REQUIRE_CALL(mock_terminal, read_key()).RETURN(std::nullopt);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(Key::Q);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(std::nullopt);
     
     SECTION("when game starts")
     {
-        SnakeGame game{mock_terminal};
+        SnakeGame game = injector.create<SnakeGame>();
         
         SECTION("menu is rendered")
         {
-            REQUIRE_CALL(mock_terminal, render_text(menu_items));
+            REQUIRE_CALL(*mock_terminal, render_text(menu_items));
             game.run();
         }
     }
@@ -127,21 +184,61 @@ TEST_CASE("SnakeGame - rendering menu")
 
 TEST_CASE("SnakeGame - game loop")
 {
-    MockTerminal mock_terminal;
+    auto injector = default_injector();
 
-    ALLOW_CALL(mock_terminal, render_text(trompeloeil::_));
-    REQUIRE_CALL(mock_terminal, read_key()).RETURN(Key::Q);
-    REQUIRE_CALL(mock_terminal, read_key()).RETURN(Key::P);
+    using MockTerminalType = decltype(injector.create<MockTerminal>());
+    auto mock_terminal = injector.create<std::shared_ptr<MockTerminalType>>();
+
+    ALLOW_CALL(*mock_terminal, render_text(trompeloeil::_));
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(Key::Q);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(Key::P);
+
+    using MockSnakeType = decltype(injector.create<MockSnake>());
+    auto mock_snake = injector.create<std::shared_ptr<MockSnakeType>>();
+    auto mock_snake_expectations = set_default_expectations(*mock_snake);
 
     SECTION("clears, renders snake & board and flushes in sequence")
     {
         trompeloeil::sequence seq;
-        REQUIRE_CALL(mock_terminal, clear()).IN_SEQUENCE(seq);
-        REQUIRE_CALL(mock_terminal, render_snake(ANY(Snake))).IN_SEQUENCE(seq);
-        REQUIRE_CALL(mock_terminal, render_board(ANY(Board))).IN_SEQUENCE(seq);
-        REQUIRE_CALL(mock_terminal, flush()).IN_SEQUENCE(seq);
+        REQUIRE_CALL(*mock_terminal, clear()).IN_SEQUENCE(seq);
+        REQUIRE_CALL(*mock_terminal, render_snake(ANY(MockSnakeType))).IN_SEQUENCE(seq);
+        REQUIRE_CALL(*mock_terminal, render_board(ANY(Board))).IN_SEQUENCE(seq);
+        REQUIRE_CALL(*mock_terminal, flush()).IN_SEQUENCE(seq);
 
-        SnakeGame game{mock_terminal};
+        SnakeGame game = injector.create<SnakeGame>();
         game.run();
+    }
+}
+
+TEST_CASE("SnakeGame - game over")
+{
+    auto injector = default_injector();
+
+    using MockTerminalType = decltype(injector.create<MockTerminal>());
+    auto mock_terminal = injector.create<std::shared_ptr<MockTerminalType>>();
+    auto mock_terminal_expectations = set_default_expectations(*mock_terminal);
+
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(Key::Q);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(std::nullopt);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(std::nullopt);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(std::nullopt);
+    REQUIRE_CALL(*mock_terminal, read_key()).RETURN(Key::P);
+
+    SECTION("when snake dies")
+    {
+        using MockSnakeType = decltype(injector.create<MockSnake>());
+        auto mock_snake = injector.create<std::shared_ptr<MockSnakeType>>();
+
+        REQUIRE_CALL(*mock_snake, is_alive()).RETURN(false);
+        REQUIRE_CALL(*mock_snake, is_alive()).RETURN(true);
+        REQUIRE_CALL(*mock_snake, is_alive()).RETURN(true);
+
+        SECTION("game over is rendered")
+        {
+            REQUIRE_CALL(*mock_terminal, render_text(std::vector<std::string>{"Game Over!!!"}));
+
+            SnakeGame game = injector.create<SnakeGame>();
+            game.run();
+        }
     }
 }
