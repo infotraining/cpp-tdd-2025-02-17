406a5fac510dfad0ebbe9ebdc1d0f0467a58a798
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-02-25 10:46:21 +0100

    Refactor DataAnalyzer and Logger classes for improved logging and data handling

--- legacy-to-testable/src/source.hpp
+++ legacy-to-testable/src/source.hpp
@@ -37,82 +37,212 @@ enum StatisticsType
     sum
 };
 
-class DataAnalyzer
+class Logger
 {
-    StatisticsType stat_type_;
-    Data data_;
-    Results results_;
-
+    Logger() = default;
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
 public:
-    DataAnalyzer(StatisticsType stat_type)
-        : stat_type_{stat_type}
+    void log(const std::string& message) 
     {
+        std::cout << "Log: " << message << std::endl;
     }
 
-    void load_data(const std::string& file_name)
+    static Logger& instance()
     {
-        data_.clear();
-        results_.clear();
-
-        std::ifstream fin(file_name.c_str());
-        if (!fin)
-            throw std::runtime_error("File not opened");
-
-        double d;
-        while (fin >> d)
-        {
-            data_.push_back(d);
-        }
-
-        std::cout << "File " << file_name << " has been loaded...\n";
-    }
-
-    void set_statistics(StatisticsType stat_type)
-    {
-        stat_type_ = stat_type;
-    }
-
-    void calculate()
-    {
-        if (stat_type_ == avg)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-            double avg = sum / data_.size();
-
-            StatResult result("Avg", avg);
-            results_.push_back(result);
-        }
-        else if (stat_type_ == min_max)
-        {
-            double min = *(std::min_element(data_.begin(), data_.end()));
-            double max = *(std::max_element(data_.begin(), data_.end()));
-
-            results_.push_back(StatResult("Min", min));
-            results_.push_back(StatResult("Max", max));
-        }
-        else if (stat_type_ == sum)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-
-            results_.push_back(StatResult("Sum", sum));
-        }
-    }
-
-    const Results& results() const
-    {
-        return results_;
-    }
-
-    void save_results(const std::string& file_name)
-    {
-        std::ofstream out{file_name};
-
-        if (!out)
-            throw std::runtime_error("Unable to open the file!!!");
-
-        for (const auto& rslt : results_)
-            std::cout << rslt.description << " = " << rslt.value << std::endl;
+        static Logger logger;
+        return logger;
     }
 };
+
+namespace Legacy
+{
+    /*inline*/ namespace ver_0
+    {
+        class DataAnalyzer
+        {
+            StatisticsType stat_type_;
+            Data data_;
+            Results results_;
+
+        public:
+            DataAnalyzer(StatisticsType stat_type)
+                : stat_type_{stat_type}
+            {
+            }
+
+            void load_data(const std::string& file_name)
+            {
+                data_.clear();
+                results_.clear();
+
+                std::ifstream fin(file_name.c_str());
+                if (!fin)
+                    throw std::runtime_error("File not opened");
+
+                double d;
+                while (fin >> d)
+                {
+                    data_.push_back(d);
+                }
+
+                Logger::instance().log("File " + file_name + " has been loaded...\n");
+            }
+
+            void set_statistics(StatisticsType stat_type)
+            {
+                stat_type_ = stat_type;
+            }
+
+            void calculate()
+            {
+                if (stat_type_ == avg)
+                {
+                    double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
+                    double avg = sum / data_.size();
+
+                    StatResult result("Avg", avg);
+                    results_.push_back(result);
+                }
+                else if (stat_type_ == min_max)
+                {
+                    double min = *(std::min_element(data_.begin(), data_.end()));
+                    double max = *(std::max_element(data_.begin(), data_.end()));
+
+                    results_.push_back(StatResult("Min", min));
+                    results_.push_back(StatResult("Max", max));
+                }
+                else if (stat_type_ == sum)
+                {
+                    double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
+
+                    results_.push_back(StatResult("Sum", sum));
+                }
+            }
+
+            const Results& results() const
+            {
+                return results_;
+            }
+
+            void save_results(const std::string& file_name)
+            {
+                std::ofstream out{file_name};
+
+                if (!out)
+                    throw std::runtime_error("Unable to open the file!!!");
+
+                for (const auto& rslt : results_)
+                    out << rslt.description << " = " << rslt.value << std::endl;
+
+                Logger::instance().log("File " + file_name + " has been saved...\n");
+            }
+        };
+    }
+
+    inline namespace ver_1
+    {
+        struct DataLoader
+        {
+            Data load_data(const std::string& file_name) const
+            {
+                Data data;
+
+                std::ifstream fin(file_name.c_str());
+                if (!fin)
+                    throw std::runtime_error("File not opened");
+
+                double d;
+                while (fin >> d)
+                {
+                    data.push_back(d);
+                }                
+
+                return data;
+            }
+        };
+
+        template <typename TDataLoader = DataLoader, typename TLogger = Logger>
+        class DataAnalyzer
+        {
+            StatisticsType stat_type_;
+            TDataLoader data_loader_;
+            TLogger& logger_;
+            Data data_;
+            Results results_;
+
+        public:
+            DataAnalyzer(StatisticsType stat_type, TDataLoader data_loader = TDataLoader{}, TLogger& logger = Logger::instance())
+                : stat_type_{stat_type}, data_loader_{data_loader}, logger_{logger}
+            {
+            }
+
+            void load_data(const std::string& file_name)
+            {
+                data_.clear();
+                results_.clear();
+
+                data_ = data_loader_.load_data(file_name);
+            
+                logger_.log("File " + file_name + " has been loaded...\n");
+            }
+
+            void set_statistics(StatisticsType stat_type)
+            {
+                stat_type_ = stat_type;
+            }
+
+            void calculate()
+            {
+                if (stat_type_ == avg)
+                {
+                    double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
+                    double avg = sum / data_.size();
+
+                    StatResult result("Avg", avg);
+                    results_.push_back(result);
+                }
+                else if (stat_type_ == min_max)
+                {
+                    double min = *(std::min_element(data_.begin(), data_.end()));
+                    double max = *(std::max_element(data_.begin(), data_.end()));
+
+                    results_.push_back(StatResult("Min", min));
+                    results_.push_back(StatResult("Max", max));
+                }
+                else if (stat_type_ == sum)
+                {
+                    double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
+
+                    results_.push_back(StatResult("Sum", sum));
+                }
+            }
+
+            const Results& results() const
+            {
+                return results_;
+            }
+
+            void save_results(const std::string& file_name)
+            {
+                do_save_results(file_name, results_);
+
+                logger_.log("File " + file_name + " has been saved...\n");
+            }
+     
+        protected:  
+            virtual void do_save_results(const std::string& file_name, const Results& results)
+            {
+                std::ofstream out{file_name};
+
+                    if (!out)
+                        throw std::runtime_error("Unable to open the file!!!");
+
+                    for (const auto& rslt : results)
+                        out << rslt.description << " = " << rslt.value << std::endl;
+            }
+        };
+    }    
+}
 
 #endif
--- legacy-to-testable/tests/source_tests.cpp
+++ legacy-to-testable/tests/source_tests.cpp
@@ -11,42 +11,82 @@
 using namespace std;
 using namespace std::literals;
 
-void save_file_content(const std::string& file_name, const std::vector<std::string>& content)
+std::string get_file_contents(const std::string& file_name)
 {
-    std::ofstream fout(file_name);
-    if (!fout)
+    std::ifstream in{file_name};
+    if (!in)
         throw std::runtime_error("File not opened!!!");
 
-    for (const auto& line : content)
-    {
-        fout << line << std::endl;
-    }
-}
-
-std::vector<std::string> load_file_content(const std::string& file_name)
-{
-    std::vector<std::string> content;
-    std::ifstream fin(file_name);
-    if (!fin)
-        throw std::runtime_error("File not opened!!!");
-
+    std::string contents;
     std::string line;
-    while (std::getline(fin, line))
+    while (std::getline(in, line))
     {
-        content.push_back(line);
+        contents += line + "\n";
     }
 
-    return content;
+    return contents;
 }
 
-TEST(Example, ExampleTest)
+TEST(Acceptance_DataAnalyzer, End_2_End)
 {
-    save_file_content("test.dat", {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
-    DataAnalyzer data_analyzer(avg);
-    data_analyzer.load_data("test.dat");
+    using namespace Legacy;
+    DataAnalyzer data_analyzer(StatisticsType::avg);
+    data_analyzer.load_data("data.dat");
+    data_analyzer.calculate();
+    data_analyzer.set_statistics(StatisticsType::min_max);
+    data_analyzer.calculate();
+    data_analyzer.set_statistics(StatisticsType::sum);
     data_analyzer.calculate();
     data_analyzer.save_results("results.txt");
 
-    auto content = load_file_content("results.txt");
-    ASSERT_EQ(content, std::vector<std::string>{"Avg = 5.5"s});
+    std::string expected_results = "Avg = 47.15\nMin = 1\nMax = 99\nSum = 4715\n";
+
+    ASSERT_EQ(get_file_contents("results.txt"), expected_results);
+}
+
+struct StubDataLoader
+{
+    Data load_data(const std::string& file_name) const
+    {
+        return {1, 2, 3, 4, 5};
+    }
+};
+
+struct SpyLogger
+{
+    std::vector<std::string> messages;
+
+    void log(const std::string& message)
+    {
+        messages.push_back(message);
+    }
+};
+
+template <typename TDataLoader, typename TLogger>
+struct TestDataAnalyzer : public Legacy::DataAnalyzer<TDataLoader, TLogger>
+{
+    using BaseClass = Legacy::DataAnalyzer<TDataLoader, TLogger>;
+    using BaseClass::BaseClass;
+
+    Results calculated_results;
+protected:
+    void do_save_results(const std::string& file_name, const Results& results) override
+    {
+        calculated_results = results;
+    }
+};
+
+TEST(UnitTest_DataAnalyzer, ReadingFile)
+{
+    using namespace Legacy;
+
+    SpyLogger logger;
+ 
+    TestDataAnalyzer data_analyzer(StatisticsType::avg, StubDataLoader{}, logger);
+    data_analyzer.load_data("data.dat");
+    data_analyzer.calculate();
+    data_analyzer.save_results("results.txt");
+
+    ASSERT_EQ(logger.messages.size(), 2);
+    ASSERT_EQ(data_analyzer.calculated_results.size(), 1);
 }
